9ed56d6fc94700188ac69fcf22ae3376cd8c0996
satin <satin.jeet@gmail.com>
2013-06-11 16:42:12 +0530

    give user choice to use python or python3

--- install.sh
+++ install.sh
@@ -13,9 +13,21 @@ then
     mkdir ~/.aliasmaker -p
     cp ./hash.hash ~/.aliasmaker/hash
     base64 -d ~/.aliasmaker/hash > ~/.aliasmaker/code.py
-    echo "
+    echo "what do you prefer ?
+1. python
+2. python3 (default)"
+    read PYTHON_VERSION
+
+    if [ "$PYTHON_VERSION" ~eq "1" ]
+    then
+      echo "
+#!/bin/sh
+python ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
+    else
+      echo "
 #!/bin/sh
 python3 ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
+    fi
     chmod a+x ~/.aliasmaker/execute
 
     sudo rm /usr/bin/createalias || echo "checking for previous links.."
