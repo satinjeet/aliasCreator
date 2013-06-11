0a34cdf6d5d543ed519f3c5987366f9fa6a60852
satin <satin.jeet@gmail.com>
2013-06-11 16:09:33 +0530

    commands for setup

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+echo "This will Install AliasMaker To Your System : (Yes => Continue, anyOther to abort)"
+read RESPONSE
+
+response1="yes"
+response2="Yes"
+response3="Y"
+response4="y"
+
+if [ "$RESPONSE" = "$response1" ] || [ "$RESPONSE" = "$response2" ] || [ "$RESPONSE" = "$response3" ] || [ "$RESPONSE" = "$response4" ]
+then
+    mkdir ~/.aliasmaker -p
+    cp ./hash.hash ~/.aliasmaker/hash
+    base64 -d ~/.aliasmaker/hash > ~/.aliasmaker/code.py
+    echo "
+#!/bin/sh
+python3 ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
+    chmod a+x ~/.aliasmaker/execute
+
+    sudo rm /usr/bin/createalias || echo "checking for previous links.."
+    echo "creating Link..".
+    sudo ln -s ~/.aliasmaker/execute /usr/bin/createalias
+    echo "process complete.."
+
+else
+    echo "aborting..."
+fi
---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+fileName="AliasMaker.py"
+base64 "$fileName" > hash.hash
