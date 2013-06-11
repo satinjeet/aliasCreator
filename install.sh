28c039bb57f77ea8f0aa8ababc2c6d56ad3b7113
satin <satin.jeet@gmail.com>
2013-06-11 16:51:59 +0530

    update install script

--- install.sh
+++ install.sh
@@ -13,28 +13,32 @@ then
     mkdir ~/.aliasmaker -p
     cp ./hash.hash ~/.aliasmaker/hash
     base64 -d ~/.aliasmaker/hash > ~/.aliasmaker/code.py
+    
     echo "what do you prefer ?
 1. python
 2. python3 (default)"
     read PYTHON_VERSION
-
-    if [ "$PYTHON_VERSION" -eq "1" ]
-    then
-      echo "
-#!/bin/sh
-python ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
-    else
-      echo "
+    
+    if [ "$PYTHON_VERSION" != "" ];then
+        if [ "$PYTHON_VERSION" -ne "1" ];
+        then
+            echo "
 #!/bin/sh
 python3 ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
+        else
+            echo "
+#!/bin/sh
+python ~/.aliasmaker/code.py" > ~/.aliasmaker/execute
+        fi
+        chmod a+x ~/.aliasmaker/execute
+        
+        sudo rm /usr/bin/createalias || echo "checking for previous links.."
+        echo "creating Link..".
+        sudo ln -s ~/.aliasmaker/execute /usr/bin/createalias
+        echo "process complete.."
+    else
+        echo "aborting..."
     fi
-    chmod a+x ~/.aliasmaker/execute
-
-    sudo rm /usr/bin/createalias || echo "checking for previous links.."
-    echo "creating Link..".
-    sudo ln -s ~/.aliasmaker/execute /usr/bin/createalias
-    echo "process complete.."
-
 else
     echo "aborting..."
 fi
