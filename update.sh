adfc54df40b292b766fb07493ed72aa187f9f90e
satin <satin.jeet@gmail.com>
2013-06-11 16:16:27 +0530

    read statement in same line

--- update.sh
+++ update.sh
@@ -4,6 +4,5 @@ fileName="AliasMaker.py"
 base64 "$fileName" > hash.hash
 git commit -a
 
-echo "your branch is: "
-read BRANCHNAME
+echo "your branch is: " read BRANCHNAME
 git push origin $BRANCHNAME
